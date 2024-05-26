5948665ee2d7b559af9f4147978e0654c07fb4d0
Dmitry Tarasov <mrsndmn46@gmail.com>
2024-05-27 00:14:40 +0300

    print device

--- tests/multimodal-llm/run_evaluation.py
+++ tests/multimodal-llm/run_evaluation.py
@@ -333,6 +333,7 @@ if __name__ == '__main__':
     output_file_name = args.out
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print("device", device)
 
     train_config = TrainConfig()
 
