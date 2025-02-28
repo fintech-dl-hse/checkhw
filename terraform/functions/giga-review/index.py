59fd60813928ad64985101425dfe71691cc8a9bc
d.tarasov <mrsndmn46@gmail.com>
2025-02-28 21:17:16 +0300

    add debug logs

--- terraform/functions/giga-review/index.py
+++ terraform/functions/giga-review/index.py
@@ -128,6 +128,13 @@ def giga_review(model, prompt, paper_link):
     file_name = paper_link_to_file_name(paper_link)
 
     print("upload to gigachat", time.time())
+
+    import logging
+    logging.basicConfig(
+        format="%(levelname)s [%(asctime)s] %(name)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        level=logging.DEBUG
+    )
     file_id = upload_to_gigachat_cloud(model, file_name, content)
 
     print("run giga review", time.time())
