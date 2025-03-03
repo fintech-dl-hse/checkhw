b447f89450607f1e5dd6b5d8c134780b22a6a6b3
d.tarasov <mrsndmn46@gmail.com>
2025-03-03 15:23:12 +0300

    long polling bot

--- terraform/functions/giga-review/build.sh
+++
@@ -1,2 +0,0 @@
-docker build . --provenance=false --platform=linux/amd64 -t cr.yandex/crpmvr751rh0p510ou6h/giga-review:latest
-docker push cr.yandex/crpmvr751rh0p510ou6h/giga-review:latest
--- terraform/functions/giga-review/index.py
+++ terraform/functions/giga-review/index.py
@@ -8,6 +8,148 @@ import requests
 from gigachat import GigaChat
 
 
+SYSTEM_PROMPT_EN = """
+Give a brief review of the scientific article. Rely solely on the provided data and facts from the text of the article. Avoid assumptions and conjectures if information is missing. Structure the review as follows:
+
+1. Title: Briefly. What is paper title?
+
+2. Affiliations: What organizations are in the authors affiliations?
+
+3. Problem: Briefly, in less than 15 words. What problem are the authors solving?
+
+4. Results: Briefly. Describe the main results.
+
+5. Methods: Briefly. Describe what the authors suggested.
+
+6. Model: Briefly. The architecture of the model. The number of parameters.
+
+7. Data: Briefly. What datasets were used in this paper?
+
+8. Strengths: Briefly. What are the advantages of the proposed method?
+
+9. Weaknesses: Briefly. What are the disadvantages of the proposed method?
+
+10. Computational resources. Briefly. Give me specific numbers.
+How many GPUs were used in the work? How many GPU hours were used for training?
+
+Follow the order and names of the points. Don't number the items. Highlight the title of each item in bold.
+Add 2 new lines after each item.
+
+Answer in English.
+"""
+
+
+def paper_link_to_file_name(paper_link):
+
+    filename = paper_link.split('/')[-1]
+    if not filename.endswith('.pdf'):
+        filename = filename + '.pdf'
+
+    return filename
+
+
+def upload_to_gigachat_cloud(model, file_name, paper_bytes):
+
+    if len(paper_bytes) > 30000000:
+        print("too large file size:", len(paper_bytes), "limit is 30MB")
+        return None
+
+    gc_file = model.upload_file((file_name, paper_bytes))
+    return gc_file.id_
+
+
+def download_paper_pdf(paper_link):
+    response = requests.get(paper_link, timeout=10)
+
+    if response.status_code == 200:
+        return response.content
+
+    print("Failed to download file", paper_link)
+    breakpoint()
+    return None
+
+
+def parse_model_outputs(model_generated_content: str):
+
+    # [ { title: "**1. Authors**", content: "Blablalba" } ]
+    parsed_content = []
+
+    current_title = None
+    current_content = ''
+    for line in model_generated_content.split("\n"):
+        if line.startswith("**"):
+            # Cut of the number
+
+            if current_title is not None:
+                # save the prev title
+                parsed_content.append({
+                    "title": current_title,
+                    "content": current_content,
+                })
+                current_content = ''
+
+            current_title = line
+        else:
+            current_content += line + '\n'
+
+    # the last one
+    parsed_content.append({
+        "title": current_title,
+        "content": current_content,
+    })
+
+    return parsed_content
+
+
+def giga_review(model, prompt, paper_link):
+
+    if '/abs/' in paper_link:
+        paper_link = paper_link.replace('/abs/', '/pdf/')
+
+    print("download paper", time.time())
+    content = download_paper_pdf(paper_link)
+    if content is None:
+        return None, "Failed to download paper"
+
+    file_name = paper_link_to_file_name(paper_link)
+
+    file_id = upload_to_gigachat_cloud(model, file_name, content)
+    print("run giga review", time.time())
+
+    result = model.chat(
+        {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": prompt,
+                    "attachments": [file_id],
+                }
+            ],
+            "temperature": 0.0
+        }
+    )
+    total_tokens = result.usage.total_tokens
+    model_output_content = result.choices[0].message.content
+
+#     total_tokens = 0
+#     model_output_content = """**Title**: Yet Another RoPE Extension Method (YaRN)
+# **Authors**: Bowen Peng, Jeffrey Quesnelle, Honglu Fan, Enrico Shippole
+# **Affiliations**: Nous Research, EleutherAI, University of Geneva
+# **Problem**: Extend the context window of large language models (LLMs) trained with Rotary Position Embeddings (RoPE) to handle longer sequences than their original pre-training allowed.
+# **Results**: The YaRN method significantly extends the context window of LLMs with RoPE, reducing computational requirements by 10x and training steps by 2.5x compared to prior methods. Fine-tuning required only 0.1% of the original pre-training data. The method surpasses state-of-the-art context window extension approaches.
+# **Methods**: YaRN employs a novel approach to interpolate RoPE embeddings, focusing on preserving high-frequency information and local distances. It includes dynamic scaling for efficient inference and achieves substantial performance without extensive fine-tuning.
+# **Model Architecture**: The architecture remains largely unchanged except for modifications to positional embeddings. Specific architectural details and parameter counts are not provided.
+# **Strengths**: Efficiency (requires less data and training time), effectiveness (surpasses state-of-the-art), and applicability (broad compatibility with libraries like Flash Attention 2).
+# **Weaknesses**: Specific to models trained with RoPE, potential sensitivity to hyperparameter tuning for optimal results.
+# **Computational Resources**: Details on GPU usage and hours are not explicitly stated in the provided excerpt."""
+    print("model returned ansver", time.time())
+    print(model_output_content)
+
+    print("\nresult total tokens:", total_tokens, "\n\n")
+
+    return model_output_content, None
+
+
 class TelegramBot():
     def __init__(self):
 
@@ -95,13 +237,61 @@ def handler(event, context):
 
 def handler_async(event_body, context):
 
-    resp = requests.post(
-        "https://bbag2h4ru428rgr0tta2.containers.yandexcloud.net/",
-        json=event_body,
-        timeout=300,
-    )
+    assert os.environ['GIGACHAT_CREDENTIALS'] is not None
 
+    gigachat_timeout = 60
+    model = GigaChat(
+        model="GigaChat-Pro",
+        scope="GIGACHAT_API_PERS",
+        verify_ssl_certs=True,
+        timeout=gigachat_timeout,
+    )
+    import httpx
+    model._client.timeout = httpx.Timeout(gigachat_timeout, connect=gigachat_timeout)
+    model._auth_client.timeout = httpx.Timeout(gigachat_timeout, connect=gigachat_timeout)
+
+
+    # paper_link = 'https://arxiv.org/pdf/2501.00544'
+
+    tbot = TelegramBot()
+
+    response_chat_id = event_body['message']['chat']['id']
+
+    message_text = event_body['message']['text']
+
+    message_id = event_body['message']['message_id']
+    resp = tbot.send_message_reaction(response_chat_id, message_id, "👀")
+    print("response_chat_id, message_id", response_chat_id, message_id)
     print("resp", resp.content)
+
+    review_text, error_text, paper_link = None, None, None
+
+    command_parts = message_text.split(' ')
+    if len(command_parts) == 0:
+        error_text = "No command provided"
+    else:
+        command = command_parts[0]
+        if command == "/review":
+            if len(command_parts) > 1:
+                paper_link = command_parts[1]
+            else:
+                error_text = "No paper link provided"
+        else:
+            error_text = "Unknown command"
+
+    print("command", command, "paper_link", paper_link)
+    review_text = 'Test ' + paper_link
+
+    print("run giga review")
+    review_text, error_text = giga_review(model, SYSTEM_PROMPT_EN, paper_link)
+
+    if review_text is not None:
+        review_text_escaped = review_text.replace('.', '\\.').replace('-', '\\-').replace('_', '\\_').replace('**', '*')
+
+        resp = tbot.send_message(chat_id=response_chat_id, message=review_text_escaped)
+        print(resp.json())
+    else:
+        tbot.send_message(chat_id=response_chat_id, message=error_text)
 
     return {
         'statusCode': 200,
@@ -110,4 +300,19 @@ def handler_async(event_body, context):
 
 if __name__ == "__main__":
 
-    pass
+    assert os.environ['GIGACHAT_CREDENTIALS'] is not None
+
+    telegram_bot_token = os.environ['TELEGRAM_BOT_TOKEN']
+
+    offset = 0
+
+    while True:
+        time.sleep(2)
+        resp = requests.get(f"https://api.telegram.org/bot{telegram_bot_token}/getUpdates?offset={offset}")
+        resp_json = resp.json()
+        print("resp_json", resp_json)
+        for update in resp_json['result']:
+            print("update", update)
+            handler_async(update, None)
+
+            offset = update['update_id'] + 1
