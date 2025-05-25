dcc729cac72bdccf821e88320c9991e68a96a6f4
d.tarasov <mrsndmn46@gmail.com>
2025-05-26 02:21:06 +0300

    update grades

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,7 +88,7 @@ resource "yandex_function" "stop-compute-tf" {
 resource "yandex_function" "homeworks-info-tf" {
     name               = "homeworks-info-tf"
     description        = "Get HTML summary grades table"
-    user_hash          = "v0.0.18"
+    user_hash          = "v0.0.19"
     runtime            = "python312"
     entrypoint         = "index.handler_summary"
     memory             = "128"
@@ -106,7 +106,7 @@ resource "yandex_function" "homeworks-info-tf" {
 resource "yandex_function" "homeworks-info-detailed-tf" {
     name               = "homeworks-info-detailed-tf"
     description        = "Get HTML detailed grades table"
-    user_hash          = "v0.0.18"
+    user_hash          = "v0.0.19"
     runtime            = "python312"
     entrypoint         = "index.handler_detailed"
     memory             = "128"
