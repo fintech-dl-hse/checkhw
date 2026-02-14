a51d3217347d4b8f43926219061d918a032489ce
d.tarasov <mrsndmn46@gmail.com>
2026-02-14 21:55:18 +0300

    wip fix time

--- terraform/functions/grades/index.py
+++ terraform/functions/grades/index.py
@@ -47,9 +47,11 @@ def _load_known_homeworks():
     with open(meta_path, encoding="utf-8") as f:
         raw = json.load(f)
     known_homeworks = {}
+    deadline_offset = datetime.timedelta(hours=3)
     for hw_id, meta in raw.items():
+        deadline = datetime.datetime.strptime(meta["deadline"], "%Y-%m-%dT%H:%M:%S")
         known_homeworks[hw_id] = {
-            "deadline": datetime.datetime.strptime(meta["deadline"], "%Y-%m-%dT%H:%M:%S"),
+            "deadline": deadline + deadline_offset,
             "bonus": meta.get("bonus", False),
         }
     return known_homeworks
