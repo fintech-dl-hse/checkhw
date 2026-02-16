73658006e5d33c39e4ff4db8e903c5b366344b8e
d.tarasov <mrsndmn46@gmail.com>
2026-02-16 18:59:01 +0300

    yc serverless functions bumpy python version, remove giga-review resources

--- terraform/main.tf
+++ terraform/main.tf
@@ -88,8 +88,8 @@ resource "yandex_function" "stop-compute-tf" {
 resource "yandex_function" "homeworks-info-tf" {
     name               = "homeworks-info-tf"
     description        = "Get HTML summary grades table"
-    user_hash          = "v0.0.55"
-    runtime            = "python312"
+    user_hash          = "v0.0.56"
+    runtime            = "python314"
     entrypoint         = "index.handler_summary"
     memory             = "128"
     execution_timeout  = "60"
@@ -106,8 +106,8 @@ resource "yandex_function" "homeworks-info-tf" {
 resource "yandex_function" "homeworks-info-detailed-tf" {
     name               = "homeworks-info-detailed-tf"
     description        = "Get HTML detailed grades table"
-    user_hash          = "v0.0.55"
-    runtime            = "python312"
+    user_hash          = "v0.0.56"
+    runtime            = "python314"
     entrypoint         = "index.handler_detailed"
     memory             = "128"
     execution_timeout  = "60"
@@ -124,8 +124,8 @@ resource "yandex_function" "homeworks-info-detailed-tf" {
 resource "yandex_function" "homeworks-info-save-fio-tf" {
     name               = "homeworks-info-save-fio-tf"
     description        = "Save FIO to ydb"
-    user_hash          = "v0.0.55"
-    runtime            = "python312"
+    user_hash          = "v0.0.56"
+    runtime            = "python314"
     entrypoint         = "index.save_nick_to_fio"
     memory             = "128"
     execution_timeout  = "60"
@@ -143,8 +143,8 @@ resource "yandex_function" "homeworks-info-save-fio-tf" {
 resource "yandex_function" "handle-github-hook-tf" {
     name               = "handle-github-hook-tf"
     description        = "Save github hook data to YDB"
-    user_hash          = "v0.0.4"
-    runtime            = "python312"
+    user_hash          = "v0.0.5"
+    runtime            = "python314"
     entrypoint         = "index.handler"
     memory             = "128"
     execution_timeout  = "60"
@@ -168,7 +168,7 @@ resource "yandex_function" "handle-github-hook-tf" {
 resource "yandex_function" "check-letters-tf" {
     name               = "check-letters-tf"
     description        = "Check letters hw submit"
-    user_hash          = "v0.0.13"
+    user_hash          = "v0.0.14"
     runtime            = "golang119"
     entrypoint         = "letters.Handler"
     memory             = "128"
@@ -183,91 +183,5 @@ resource "yandex_function" "check-letters-tf" {
     }
     content {
         zip_filename = "functions/letters.zip"
-    }
-}
-
-
-resource "yandex_message_queue" "ymq-giga-review-success-tf" {
-  name                       = "ymq-giga-review-success-tf"
-  visibility_timeout_seconds = 600
-  receive_wait_time_seconds  = 20
-  message_retention_seconds  = 1209600
-}
-
-resource "yandex_message_queue" "ymq-giga-review-failure-tf" {
-  name                       = "ymq-giga-review-failure-tf"
-  visibility_timeout_seconds = 600
-  receive_wait_time_seconds  = 20
-  message_retention_seconds  = 1209600
-}
-
-resource "yandex_function" "giga-review-queue-tf" {
-    name               = "giga-review-queue-tf"
-    description        = "Giga review queue"
-    user_hash          = "v0.0.32"
-    runtime            = "python39"
-    entrypoint         = "index.handler"
-    memory             = "512"
-    execution_timeout  = "10"
-    service_account_id = "ajevd0tfv30vuibuhv6v"
-    secrets {
-        id                   = "e6qcoml5i8pd749m2pi7"
-        version_id           = "e6q6fukbt0r6bdh6qege"
-        key                  = "TELEGRAM_BOT_TOKEN"
-        environment_variable = "TELEGRAM_BOT_TOKEN"
-    }
-    secrets {
-        id                   = "e6qabpm2adbfjq16919k"
-        version_id           = "e6qkeviqvd3jvo1jniju"
-        key                  = "TELEGRAM_BOT_WEBHOOK_SECRET_TOKEN"
-        environment_variable = "TELEGRAM_BOT_WEBHOOK_SECRET_TOKEN"
-    }
-    content {
-        zip_filename = "functions/giga-review.zip"
-    }
-}
-
-
-resource "yandex_function" "giga-review-tf" {
-    name               = "giga-review-tf"
-    description        = "Giga review"
-    user_hash          = "v0.0.32"
-    runtime            = "python39"
-    entrypoint         = "index.handler_async"
-    memory             = "512"
-    execution_timeout  = "300"
-    service_account_id = "ajevd0tfv30vuibuhv6v"
-    secrets {
-        id                   = "e6qcoml5i8pd749m2pi7"
-        version_id           = "e6q6fukbt0r6bdh6qege"
-        key                  = "TELEGRAM_BOT_TOKEN"
-        environment_variable = "TELEGRAM_BOT_TOKEN"
-    }
-    secrets {
-        id                   = "e6qsp6kj884e4tfgvcrq"
-        version_id           = "e6qg7466ek0qt8k5o0d0"
-        key                  = "GIGACHAT_CREDENTIALS"
-        environment_variable = "GIGACHAT_CREDENTIALS"
-    }
-    secrets {
-        id                   = "e6qabpm2adbfjq16919k"
-        version_id           = "e6qkeviqvd3jvo1jniju"
-        key                  = "TELEGRAM_BOT_WEBHOOK_SECRET_TOKEN"
-        environment_variable = "TELEGRAM_BOT_WEBHOOK_SECRET_TOKEN"
-    }
-    content {
-        zip_filename = "functions/giga-review.zip"
-    }
-    async_invocation {
-        retries_count       = 1
-        service_account_id  = "ajevd0tfv30vuibuhv6v"
-        ymq_success_target {
-            service_account_id = "ajevd0tfv30vuibuhv6v"
-            arn                = "yrn:yc:ymq:ru-central1:b1gtgl0ktbrjt750ihta:ymq-giga-review-success-tf"
-        }
-        ymq_failure_target {
-            service_account_id = "ajevd0tfv30vuibuhv6v"
-            arn                = "yrn:yc:ymq:ru-central1:b1gtgl0ktbrjt750ihta:ymq-giga-review-failure-tf"
-        }
     }
 }
